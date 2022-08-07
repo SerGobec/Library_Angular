15d789fac7d79f812968c96e34615c4f4cd2c288
SerGobec <48455848+SerGobec@users.noreply.github.com>
2022-08-07 17:12:43 +0300

    added required components

--- LibraryApp/src/app/app.module.ts
+++ LibraryApp/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BooksPageComponent } from './books-page/books-page.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
+import { ViewBookComponent } from './view-book/view-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksPageComponent,
+    BookListComponent,
+    BookListItemComponent,
+    EditBookComponent,
+    ViewBookComponent
   ],
   imports: [
     BrowserModule
